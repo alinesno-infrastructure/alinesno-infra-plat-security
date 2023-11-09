85086aaf5d9199044c6ca819c324654b552d87fd
CarrotSea233 <2733614757@qq.com>
2023-11-09 21:10:00 +0800

    feat: 集成mybatis-actable

--- alinesno-infra-plat-security-boot/src/main/java/com/alinesno/infra/plat/security/PlatSecurityApplication.java
+++ alinesno-infra-plat-security-boot/src/main/java/com/alinesno/infra/plat/security/PlatSecurityApplication.java
@@ -1,5 +1,6 @@
 package com.alinesno.infra.plat.security;
 
+import com.alinesno.infra.common.facade.enable.EnableActable;
 import com.alinesno.infra.common.web.adapter.sso.enable.EnableInfraSsoApi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,8 +11,9 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
  * @author LuoAnDong
  * @since 2023年8月3日 上午6:23:43
  */
+@EnableActable
 @EnableInfraSsoApi
-@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@SpringBootApplication
 public class PlatSecurityApplication {
 
 	public static void main(String[] args) {
