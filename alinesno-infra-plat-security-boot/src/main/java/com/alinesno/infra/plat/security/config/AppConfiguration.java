cea01a68a4ba705dcec0833e9bc6bbb4077a34ca
李伟(Michael) <michaelAip@gmail.com>
2024-12-03 00:38:46 +0800

    feat:在 AppConfiguration 中添加日志调试及 CommandLineRunner 实现

--- alinesno-infra-plat-security-boot/src/main/java/com/alinesno/infra/plat/security/config/AppConfiguration.java
+++ alinesno-infra-plat-security-boot/src/main/java/com/alinesno/infra/plat/security/config/AppConfiguration.java
@@ -3,18 +3,26 @@ package com.alinesno.infra.plat.security.config;
 import com.alinesno.infra.common.facade.enable.EnableActable;
 import com.alinesno.infra.common.web.adapter.sso.enable.EnableInfraSsoApi;
 import com.alinesno.infra.common.web.log.aspect.LogAspect;
+import com.dtflys.forest.springboot.annotation.ForestScan;
+import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
  * 配置管理中心
  */
+@Slf4j
 @EnableActable
 @MapperScan("com.alinesno.infra.plat.security.mapper")
+@ForestScan({
+        "com.alinesno.infra.common.web.adapter.base.consumer" ,
+        "com.alinesno.infra.base.platform.adapter"
+})
 @EnableInfraSsoApi
 @Configuration
-public class AppConfiguration {
+public class AppConfiguration implements CommandLineRunner {
 
     /**
      * 日志操作拦截
@@ -23,6 +31,11 @@ public class AppConfiguration {
     @Bean
     public LogAspect getLogAspect(){
         return new LogAspect() ;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        log.debug("AppConfiguration.run");
     }
 
 }
